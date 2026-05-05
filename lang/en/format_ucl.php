3a150fdd98c7f7540eb8fe0cc3d50e039c56d93d
Stuart Lamour <thisismyrice@gmail.com>
2026-05-05 20:34:08 +0100

    lang string

--- lang/en/format_ucl.php
+++ lang/en/format_ucl.php
@@ -26,6 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['addactivity'] = 'Add activity or resource';
+$string['addnewsection'] = 'Add a new section';
 $string['addsectiondescription'] = 'Add section description';
 $string['addsections'] = 'Add section';
 $string['addtextmedia'] = 'Add text and media';

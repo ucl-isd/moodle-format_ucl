458548a565f899dff664ec3541eceb1a58aa5538
amandadoughty <m.doughty@ucl.ac.uk>
2026-05-18 13:25:18 +0100

    CTP-6273 Verson # bump

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'format_ucl';
 $plugin->release = '0.1.0';
-$plugin->version = 2026051300;
+$plugin->version = 2026051800;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_BETA;

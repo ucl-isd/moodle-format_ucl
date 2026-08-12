ucl-isd/moodle-format_ucl

commit 804219120afdb0f01ca7a00d9d7de07eb9c4995d

Ctp 6451 refactor progress mdl 60912 (#52)
* CTP-6541 Refactored progress to use back ported function form MDL_60912

CTP-6541 Added behat test

CTP-6541 Added skip step to progress.feature as it fails without backported commits

CTP-6541 Code checker fixes

* CTP-6451 Recomplied JS

* CTP-6451 Code review changes

* CTP-6451 Bumped version #

classes/output/widgets/toc.php
@@ -54,14 +54,14 @@ class toc implements renderable, templatable {
      * @param renderer_base $output
      */
     public function export_for_template(renderer_base $output) {
-        global $PAGE, $USER, $CFG;
+        global $USER, $CFG;
         if (!$course = $this->format->get_course()) {
             return [];
         }
 
         $activesection = optional_param('id', 0, PARAM_INT);
         $context = context_course::instance($course->id);
-        $caneditcourse = has_capability('moodle/course:update', $context);
+        $canviewhidden = has_capability('moodle/course:update', $context);
         $coursesections = $this->format->get_sections();
         $currentsectionnum = $this->format->get_sectionnum();
 
@@ -71,7 +71,31 @@ class toc implements renderable, templatable {
 
         $data = new stdClass();
         foreach ($coursesections as $section) {
-            if ($section->uservisible || $caneditcourse) {
+            // Editor warning data.
+            // Don't count section 0.
+            if ($canviewhidden && $section->section && $section->visible) {
+                $visiblecount++;
+
+                // Sections without a name.
+                if (!$section->name) {
+                    $namecount++;
+                }
+
+                // Sections with one or less mods.
+                $modinfo = $this->format->get_modinfo();
+                $cmids = $modinfo->sections[$section->section] ?? [];
+                if (count($cmids) < 2) {
+                    $modcount++;
+                }
+
+                // Sections with lots of mods, and no labels.
+                // phpcs:disable Generic.CodeAnalysis.EmptyStatement.DetectedIf
+                if (count($cmids) > 5) {
+                    // TODO - not sure yet.
+                }
+            }
+
+            if ($section->uservisible || $canviewhidden) {
                 $s = new stdClass();
                 $s->id = $section->id;
                 $s->section = $section->section;
@@ -94,45 +118,13 @@ class toc implements renderable, templatable {
                 // Progress.
                 if ($course->enablecompletion) {
                     if (!$USER->editing) {
-                        $s->progress = $this->format_ucl_section_progress($section);
+                        $s->progress = self::format_ucl_section_progress($section, $course);
                     }
                 }
 
                 // Add to template data.
                 $data->coursesection[] = $s;
             }
-
-            // Editor warning data.
-            if (!$caneditcourse) {
-                continue;
-            }
-            if ($section->section) { // Don't count section 0.
-                if ($section->visible) {
-                    $visiblecount++;
-
-                    // Sections without a name.
-                    if (!$section->name) {
-                        $namecount++;
-                    }
-
-                    // Sections with one or less mods.
-                    $modinfo = $this->format->get_modinfo();
-                    $cmids = $modinfo->sections[$section->section] ?? [];
-                    if (count($cmids) < 2) {
-                        $modcount++;
-                    }
-
-                    // Sections with lots of mods, and no labels.
-                    // phpcs:disable Generic.CodeAnalysis.EmptyStatement.DetectedIf
-                    if (count($cmids) > 5) {
-                        // TODO - not sure yet.
-                    }
-                }
-            }
-        }
-
-        if (!$caneditcourse) {
-            return $data;
         }
 
         // Editor warnings.
@@ -205,46 +197,60 @@ class toc implements renderable, templatable {
      * Given a section, return the data for progress.
      *
      * @param section_info $section
-     * @return stdClass
+     * @param stdClass $course
+     * @param int|null $userid
+     * @return stdClass|null
      */
-    public function format_ucl_section_progress(section_info $section): stdClass {
-        $course = $this->format->get_course();
-        // Get all the Moodle things.
-        $modinfo = $this->format->get_modinfo();
-        $completioninfo = new completion_info($course);
-        $cmids = $modinfo->sections[$section->section] ?? [];
+    public static function format_ucl_section_progress(
+        section_info $section,
+        stdClass $course,
+        ?int $userid = null
+    ): ?stdClass {
+        global $USER;
 
-        // Count vars.
-        $total = 0;
-        $complete = 0;
-
-        // Loop through cm in this section.
-        foreach ($cmids as $cmid) {
-            $thismod = $modinfo->cms[$cmid];
-            if ($thismod->uservisible) {
-                if ($completioninfo->is_enabled($thismod) != COMPLETION_TRACKING_NONE) {
-                    $total++;
-                    $completiondata = $completioninfo->get_data($thismod, true);
-                    if (
-                        $completiondata->completionstate == COMPLETION_COMPLETE ||
-                        $completiondata->completionstate == COMPLETION_COMPLETE_PASS
-                    ) {
-                        $complete++;
-                    }
-                }
-            }
+        // Make sure we continue with a valid userid.
+        if (empty($userid)) {
+            $userid = $USER->id;
         }
+
+        // Get all the Moodle things.
+        $cmids = explode(',', $section->sequence);
+        $completion = new \completion_info($course);
+
+        // First, let's make sure completion is enabled.
+        if (!$completion->is_enabled()) {
+            return null;
+        }
+
+        if (!$completion->is_tracked_user($userid)) {
+            return null;
+        }
+
+        static $activitieswithcompletion = null;
+
+        // Get the modules that support completion.
+        if (is_null($activitieswithcompletion)) {
+            $activitieswithcompletion = $completion->get_user_activities_with_completion($userid);
+        }
+
+        // Only include the modules in this section.
+        $modules = array_intersect_key($activitieswithcompletion, array_flip($cmids));
+
+        if (!$total = count($modules)) {
+            return null;
+        }
+
+        // Get the number of modules that have been completed.
+        $complete = $completion->count_modules_completed($userid, array_keys($modules));
 
         // Return data.
         $data = new stdClass();
-        if ($total) {
-            $data->id = $section->id;
-            $data->total = $total;
-            $data->complete = $complete;
-            $data->percentage = round(($complete / $total) * 100);
-            if ($data->percentage == 100) {
-                $data->done = true;
-            }
+        $data->id = $section->id;
+        $data->total = $total;
+        $data->complete = $complete;
+        $data->percentage = round(($complete / $total) * 100);
+        if ($data->percentage == 100) {
+            $data->done = true;
         }
         return $data;
     }

amd/src/local/toc.js
@@ -24,9 +24,9 @@
  */
 
 import {BaseComponent} from 'core/reactive';
-import CourseEvents from 'core_course/events';
 import {getCurrentCourseEditor} from 'core_courseformat/courseeditor';
 import {get_string as getString} from 'core/str';
+import Ajax from 'core/ajax';
 
 export default class Component extends BaseComponent {
 
@@ -64,59 +64,34 @@ export default class Component extends BaseComponent {
         sections.forEach((section) => {
             this.sections[section.dataset.id] = section;
         });
+    }
 
-        this._initSectionProgress();
+    getWatchers() {
+        return [
+            {watch: `section:deleted`, handler: this._deleteSection},
+            // Sections and cm sorting.
+            {watch: `course.sectionlist:updated`, handler: this._refreshCourseToc},
+            // Progress updates.
+            {watch: `cm.completionstate:updated`, handler: this._refreshCompletion},
+        ];
     }
 
     /**
-     * Section progress updates on load and when completion is toggled.
-     */
-    _initSectionProgress() {
-        const manualCompletionEvent = CourseEvents.manualCompletionEvent
-            || CourseEvents.manualCompletionToggled
-            || 'core_course:manualcompletiontoggled';
-
-        document.addEventListener(manualCompletionEvent, (event) => {
-            const detail = event.detail || {};
-            const cm = this.reactive.get('cm', detail.cmid);
-            if (!cm?.sectionid) {
-                return;
-            }
-
-            this._updateSectionProgress(cm.sectionid);
-        });
-
-        this.element.querySelectorAll('.progress-indicator[data-id]').forEach((indicator) => {
-            this._updateSectionProgress(indicator.dataset.id);
-        });
-    }
-
-    /**
-     * Count done items in one section, then pass the totals to the UI.
+     * Section progress updates when completion is toggled.
      *
-     * @param {string|number} sectionId the section id
+     * @param {Object} details the update details.
+     * @param {Object} details.element the element data.
      */
-    _updateSectionProgress(sectionId) {
-        const section = this.reactive.get('section', sectionId);
-        if (!section) {
-            return;
-        }
+    async _refreshCompletion({element}) {
+        const cm = this.reactive.get('cm', element.id);
+        const sectionId = cm.sectionid;
+        const progress = await Ajax.call([{
+            methodname: 'format_ucl_get_section_progress',
+            args: {sectionid: sectionId}
+        }])[0];
 
-        const cmlist = section.cmlist || [];
-        let total = 0;
-        let done = 0;
-
-        cmlist.forEach((cmId) => {
-            const cm = this.reactive.get('cm', cmId);
-            if (!cm || typeof cm.completionstate === 'undefined') {
-                return;
-            }
-
-            total++;
-            if (cm.isoverallcomplete === true || cm.completionstate === 1 || cm.completionstate === 2) {
-                done++;
-            }
-        });
+        const total = progress.total;
+        const done = progress.completed;
 
         this._renderProgress({sectionId, total, done});
     }
@@ -178,14 +153,6 @@ export default class Component extends BaseComponent {
                 clearInterval(interval);
             }
         }, 20);
-    }
-
-    getWatchers() {
-        return [
-            {watch: `section:deleted`, handler: this._deleteSection},
-            // Sections and cm sorting.
-            {watch: `course.sectionlist:updated`, handler: this._refreshCourseToc},
-        ];
     }
 
     /**

classes/external/get_section_progress.php
@@ -1,0 +1,90 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace format_ucl\external;
+
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_single_structure;
+use core_external\external_value;
+use format_ucl\output\widgets\toc;
+
+/**
+ * Gets the users progress for a section
+ *
+ * @package     format_ucl
+ * @copyright   2026 onwards University College London {@link https://www.ucl.ac.uk/}
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author      Stuart Lamour <s.lamour@ucl.ac.uk>
+ * @author      Amanda Doughty <m.doughty@ucl.ac.uk>
+ */
+class get_section_progress extends external_api {
+    /**
+     * Parameters for this webservice function.
+     *
+     * @return external_function_parameters
+     */
+    public static function execute_parameters(): external_function_parameters {
+        return new external_function_parameters([
+            'sectionid' => new external_value(PARAM_INT, 'the section'),
+        ]);
+    }
+
+    /**
+     * Get the users progress for the section
+     *
+     * @param int $sectionid
+     * @return array
+     */
+    public static function execute(int $sectionid): array {
+        global $DB;
+        // Clean params.
+        ['sectionid' => $sectionid] =
+            self::validate_parameters(
+                self::execute_parameters(),
+                ['sectionid' => $sectionid]
+            );
+
+        $courseid = $DB->get_field('course_sections', 'course', ['id' => $sectionid]);
+        $course = get_course($courseid);
+        $context = \context_course::instance($courseid);
+        self::validate_context($context);
+
+        $modinfo = get_fast_modinfo($course);
+        $section = $modinfo->get_section_info_by_id($sectionid);
+
+        $total = 0;
+        $completed = 0;
+        if ($progress = toc::format_ucl_section_progress($section, $course)) {
+            $total = $progress->total;
+            $completed = $progress->complete;
+        }
+
+        return ['total' => $total, 'completed' => $completed];
+    }
+
+    /**
+     * Return structure for this webservice function.
+     *
+     * @return external_single_structure
+     */
+    public static function execute_returns(): external_single_structure {
+        return new external_single_structure([
+            'total' => new external_value(PARAM_INT, 'Number of activities with completion'),
+            'completed' => new external_value(PARAM_INT, 'Number of completed activities'),
+        ]);
+    }
+}

db/services.php
@@ -24,6 +24,7 @@
  * @author      Amanda Doughty <m.doughty@ucl.ac.uk>
  */
 
+use format_ucl\external\get_section_progress;
 use format_ucl\external\set_contact_visibility;
 
 defined('MOODLE_INTERNAL') || die();
@@ -34,5 +35,13 @@ $functions = [
         'description' => '',
         'type' => 'write',
         'ajax' => true,
+    ],
+    'format_ucl_get_section_progress' => [
+        'classname' => get_section_progress::class,
+        'description' => 'Gets the total number and number completed of visible activities
+                          that are not restricted by group',
+        'type' => 'read',
+        'ajax' => true,
+        'readonlysession' => true,
     ],
 ];

tests/behat/progress.feature
@@ -5,10 +5,14 @@ Feature: TOC progress is shown on load and updates after manual completion
   I need to see correct progress text on page load and after completion changes
 
   Background:
-    Given the following "users" exist:
+    Given the site is running Moodle version 99 or higher
+    # The following steps should not be executed for 4.5. If they are, the test will fail.
+    # We have backported a fix that they rely on.
+    And the following "users" exist:
       | username | firstname | lastname | email                |
       | teacher1 | Teacher   | 1        | teacher1@example.com |
       | student1 | Student   | 1        | student1@example.com |
+      | student2 | Student   | 2        | student2@example.com |
     And the following "course" exists:
       | fullname         | Course 1 |
       | shortname        | PROG1    |
@@ -21,11 +25,18 @@ Feature: TOC progress is shown on load and updates after manual completion
       | user     | course | role           |
       | teacher1 | PROG1  | editingteacher |
       | student1 | PROG1  | student        |
+      | student2 | PROG1  | student        |
+    And the following "groups" exist:
+      | course | idnumber | name     |
+      | PROG1  | G1       | My group |
+    And the following "group members" exist:
+      | group | user     |
+      | G1    | student1 |
     And the following "activities" exist:
-      | activity | course | idnumber | name               | section | completion |
-      | page     | PROG1  | page1    | Activity sample 1  | 2       | 1          |
-      | page     | PROG1  | page2    | Activity sample 2  | 2       | 1          |
-      | page     | PROG1  | page0    | Activity sample 0  | 0       | 1          |
+      | activity | course | idnumber | name              | section | completion |
+      | page     | PROG1  | page1    | Activity sample 1 | 2       | 1          |
+      | page     | PROG1  | page2    | Activity sample 2 | 2       | 1          |
+      | page     | PROG1  | page0    | Activity sample 0 | 0       | 1          |
     And I change window size to "large"
 
   Scenario: TOC progress is not output when course completion is disabled
@@ -60,21 +71,38 @@ Feature: TOC progress is shown on load and updates after manual completion
     Given I am on the "PROG1" "Course" page logged in as "student1"
     And I click on "Section 2" "link" in the "#toc" "css_element"
     And "#toc [data-section='2'] .progress-indicator[data-id] .pie" "css_element" should exist
-    And I wait until "Mark as done" "button" exists
     And the "data-behat-percentage" attribute of "#toc [data-section='2'] .progress-indicator[data-id] .pie" "css_element" should contain "0"
-    When I press "Mark as done"
-    And I wait until "Done" "button" exists
+    When I toggle the manual completion state of "Activity sample 2"
     Then the "data-behat-percentage" attribute of "#toc [data-section='2'] .progress-indicator[data-id] .pie" "css_element" should contain "50"
-    When I reload the page
-    Then the "data-behat-percentage" attribute of "#toc [data-section='2'] .progress-indicator[data-id] .pie" "css_element" should contain "50"
+    And I reload the page
+    And the "data-behat-percentage" attribute of "#toc [data-section='2'] .progress-indicator[data-id] .pie" "css_element" should contain "50"
 
   Scenario: TOC progress in section 0 updates after manual completion
     Given I am on the "PROG1" "Course" page logged in as "student1"
     And "#toc [data-section='0'] .progress-indicator[data-id] .pie" "css_element" should exist
-    And I wait until "Mark as done" "button" exists
     And the "data-behat-percentage" attribute of "#toc [data-section='0'] .progress-indicator[data-id] .pie" "css_element" should contain "0"
-    When I press "Mark as done"
-    And I wait until "Done" "button" exists
+    When I toggle the manual completion state of "Activity sample 0"
     Then the "data-behat-percentage" attribute of "#toc [data-section='0'] .progress-indicator[data-id] .pie" "css_element" should contain "100"
-    When I reload the page
-    Then the "data-behat-percentage" attribute of "#toc [data-section='0'] .progress-indicator[data-id] .pie" "css_element" should contain "100"
+    And I reload the page
+    And the "data-behat-percentage" attribute of "#toc [data-section='0'] .progress-indicator[data-id] .pie" "css_element" should contain "100"
+
+  Scenario: TOC progress does not include activities restricted by group
+    Given I log in as "teacher1"
+    And I am on the "page1" "page activity editing" page
+    And I expand all fieldsets
+    And I click on "Add restriction..." "button"
+    And I click on "Group" "button" in the "Add restriction..." "dialogue"
+    And I set the field "Group" to "My group"
+    And I click on ".availability-item .availability-eye img" "css_element"
+    And I click on "Save and return to course" "button"
+    And I log out
+    When I am on the "PROG1" "Course" page logged in as "student1"
+    And I click on "Section 2" "link" in the "#toc" "css_element"
+    And I toggle the manual completion state of "Activity sample 2"
+    # 1/2 activities completed
+    Then the "data-behat-percentage" attribute of "#toc [data-section='2'] .progress-indicator[data-id] .pie" "css_element" should contain "50"
+    # 1/1 activities completed - page1 is not available to student2 because of the group restriction
+    When I am on the "PROG1" "Course" page logged in as "student2"
+    And I click on "Section 2" "link" in the "#toc" "css_element"
+    And I toggle the manual completion state of "Activity sample 2"
+    Then the "data-behat-percentage" attribute of "#toc [data-section='2'] .progress-indicator[data-id] .pie" "css_element" should contain "100"

version.php
@@ -28,6 +28,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'format_ucl';
 $plugin->release = '0.1.0';
-$plugin->version = 2026072101;
+$plugin->version = 2026072300;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_BETA;

commit d79ce56effcdb703b9bdf88aae5c988eecc0dd4c

CTP-6661 Fixed regression from merge conflict in PR #52

classes/output/widgets/toc.php
@@ -61,7 +61,7 @@ class toc implements renderable, templatable {
 
         $activesection = optional_param('id', 0, PARAM_INT);
         $context = context_course::instance($course->id);
-        $canviewhidden = has_capability('moodle/course:update', $context);
+        $caneditcourse = has_capability('moodle/course:update', $context);
         $coursesections = $this->format->get_sections();
         $currentsectionnum = $this->format->get_sectionnum();
 
@@ -71,31 +71,7 @@ class toc implements renderable, templatable {
 
         $data = new stdClass();
         foreach ($coursesections as $section) {
-            // Editor warning data.
-            // Don't count section 0.
-            if ($canviewhidden && $section->section && $section->visible) {
-                $visiblecount++;
-
-                // Sections without a name.
-                if (!$section->name) {
-                    $namecount++;
-                }
-
-                // Sections with one or less mods.
-                $modinfo = $this->format->get_modinfo();
-                $cmids = $modinfo->sections[$section->section] ?? [];
-                if (count($cmids) < 2) {
-                    $modcount++;
-                }
-
-                // Sections with lots of mods, and no labels.
-                // phpcs:disable Generic.CodeAnalysis.EmptyStatement.DetectedIf
-                if (count($cmids) > 5) {
-                    // TODO - not sure yet.
-                }
-            }
-
-            if ($section->uservisible || $canviewhidden) {
+            if ($section->uservisible || $caneditcourse) {
                 $s = new stdClass();
                 $s->id = $section->id;
                 $s->section = $section->section;
@@ -125,8 +101,39 @@ class toc implements renderable, templatable {
                 // Add to template data.
                 $data->coursesection[] = $s;
             }
+
+            // Editor warning data.
+            if (!$caneditcourse) {
+                continue;
+            }
+            if ($section->section) { // Don't count section 0.
+                if ($section->visible) {
+                    $visiblecount++;
+
+                    // Sections without a name.
+                    if (!$section->name) {
+                        $namecount++;
+                    }
+
+                    // Sections with one or less mods.
+                    $modinfo = $this->format->get_modinfo();
+                    $cmids = $modinfo->sections[$section->section] ?? [];
+                    if (count($cmids) < 2) {
+                        $modcount++;
+                    }
+
+                    // Sections with lots of mods, and no labels.
+                    // phpcs:disable Generic.CodeAnalysis.EmptyStatement.DetectedIf
+                    if (count($cmids) > 5) {
+                        // TODO - not sure yet.
+                    }
+                }
+            }
         }
 
+        if (!$caneditcourse) {
+            return $data;
+        }
         // Editor warnings.
         $data->showwarning = false;
         $data->showguidance = false;

tests/behat/warnings.feature
@@ -120,11 +120,13 @@ Feature: Appropriate Tips are shown to user
 
   Scenario: Users without capability 'moodle/course:update' do not see warnings
     When I am on the "C2" course page logged in as "teacher1"
-    Then ".behat-unnamedsections" "css_element" should exist
+    Then ".behat-nocourseimg" "css_element" should exist
+    And ".behat-unnamedsections" "css_element" should exist
     And ".behat-toomanysections" "css_element" should exist
     And ".behat-toofewmods" "css_element" should exist
     And ".behat-linktoguidance" "css_element" should exist
     When I am on the "C2" course page logged in as "student1"
+    And ".behat-nocourseimg" "css_element" should not exist
     Then ".behat-unnamedsections" "css_element" should not exist
     And ".behat-toomanysections" "css_element" should not exist
     And ".behat-toofewmods" "css_element" should not exist
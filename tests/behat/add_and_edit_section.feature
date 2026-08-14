ucl-isd/moodle-format_ucl

commit 804219120afdb0f01ca7a00d9d7de07eb9c4995d

Ctp 6451 refactor progress mdl 60912 (#52)
* CTP-6541 Refactored progress to use back ported function form MDL_60912

CTP-6541 Added behat test

CTP-6541 Added skip step to progress.feature as it fails without backported commits

CTP-6541 Code checker fixes

* CTP-6451 Recomplied JS

* CTP-6451 Code review changes

* CTP-6451 Bumped version #

classes/output/widgets/toc.php
@@ -54,14 +54,14 @@ class toc implements renderable, templatable {
      * @param renderer_base $output
      */
     public function export_for_template(renderer_base $output) {
-        global $PAGE, $USER, $CFG;
+        global $USER, $CFG;
         if (!$course = $this->format->get_course()) {
             return [];
         }
 
         $activesection = optional_param('id', 0, PARAM_INT);
         $context = context_course::instance($course->id);
-        $caneditcourse = has_capability('moodle/course:update', $context);
+        $canviewhidden = has_capability('moodle/course:update', $context);
         $coursesections = $this->format->get_sections();
         $currentsectionnum = $this->format->get_sectionnum();
 
@@ -71,7 +71,31 @@ class toc implements renderable, templatable {
 
         $data = new stdClass();
         foreach ($coursesections as $section) {
-            if ($section->uservisible || $caneditcourse) {
+            // Editor warning data.
+            // Don't count section 0.
+            if ($canviewhidden && $section->section && $section->visible) {
+                $visiblecount++;
+
+                // Sections without a name.
+                if (!$section->name) {
+                    $namecount++;
+                }
+
+                // Sections with one or less mods.
+                $modinfo = $this->format->get_modinfo();
+                $cmids = $modinfo->sections[$section->section] ?? [];
+                if (count($cmids) < 2) {
+                    $modcount++;
+                }
+
+                // Sections with lots of mods, and no labels.
+                // phpcs:disable Generic.CodeAnalysis.EmptyStatement.DetectedIf
+                if (count($cmids) > 5) {
+                    // TODO - not sure yet.
+                }
+            }
+
+            if ($section->uservisible || $canviewhidden) {
                 $s = new stdClass();
                 $s->id = $section->id;
                 $s->section = $section->section;
@@ -94,45 +118,13 @@ class toc implements renderable, templatable {
                 // Progress.
                 if ($course->enablecompletion) {
                     if (!$USER->editing) {
-                        $s->progress = $this->format_ucl_section_progress($section);
+                        $s->progress = self::format_ucl_section_progress($section, $course);
                     }
                 }
 
                 // Add to template data.
                 $data->coursesection[] = $s;
             }
-
-            // Editor warning data.
-            if (!$caneditcourse) {
-                continue;
-            }
-            if ($section->section) { // Don't count section 0.
-                if ($section->visible) {
-                    $visiblecount++;
-
-                    // Sections without a name.
-                    if (!$section->name) {
-                        $namecount++;
-                    }
-
-                    // Sections with one or less mods.
-                    $modinfo = $this->format->get_modinfo();
-                    $cmids = $modinfo->sections[$section->section] ?? [];
-                    if (count($cmids) < 2) {
-                        $modcount++;
-                    }
-
-                    // Sections with lots of mods, and no labels.
-                    // phpcs:disable Generic.CodeAnalysis.EmptyStatement.DetectedIf
-                    if (count($cmids) > 5) {
-                        // TODO - not sure yet.
-                    }
-                }
-            }
-        }
-
-        if (!$caneditcourse) {
-            return $data;
         }
 
         // Editor warnings.
@@ -205,46 +197,60 @@ class toc implements renderable, templatable {
      * Given a section, return the data for progress.
      *
      * @param section_info $section
-     * @return stdClass
+     * @param stdClass $course
+     * @param int|null $userid
+     * @return stdClass|null
      */
-    public function format_ucl_section_progress(section_info $section): stdClass {
-        $course = $this->format->get_course();
-        // Get all the Moodle things.
-        $modinfo = $this->format->get_modinfo();
-        $completioninfo = new completion_info($course);
-        $cmids = $modinfo->sections[$section->section] ?? [];
+    public static function format_ucl_section_progress(
+        section_info $section,
+        stdClass $course,
+        ?int $userid = null
+    ): ?stdClass {
+        global $USER;
 
-        // Count vars.
-        $total = 0;
-        $complete = 0;
-
-        // Loop through cm in this section.
-        foreach ($cmids as $cmid) {
-            $thismod = $modinfo->cms[$cmid];
-            if ($thismod->uservisible) {
-                if ($completioninfo->is_enabled($thismod) != COMPLETION_TRACKING_NONE) {
-                    $total++;
-                    $completiondata = $completioninfo->get_data($thismod, true);
-                    if (
-                        $completiondata->completionstate == COMPLETION_COMPLETE ||
-                        $completiondata->completionstate == COMPLETION_COMPLETE_PASS
-                    ) {
-                        $complete++;
-                    }
-                }
-            }
+        // Make sure we continue with a valid userid.
+        if (empty($userid)) {
+            $userid = $USER->id;
         }
+
+        // Get all the Moodle things.
+        $cmids = explode(',', $section->sequence);
+        $completion = new \completion_info($course);
+
+        // First, let's make sure completion is enabled.
+        if (!$completion->is_enabled()) {
+            return null;
+        }
+
+        if (!$completion->is_tracked_user($userid)) {
+            return null;
+        }
+
+        static $activitieswithcompletion = null;
+
+        // Get the modules that support completion.
+        if (is_null($activitieswithcompletion)) {
+            $activitieswithcompletion = $completion->get_user_activities_with_completion($userid);
+        }
+
+        // Only include the modules in this section.
+        $modules = array_intersect_key($activitieswithcompletion, array_flip($cmids));
+
+        if (!$total = count($modules)) {
+            return null;
+        }
+
+        // Get the number of modules that have been completed.
+        $complete = $completion->count_modules_completed($userid, array_keys($modules));
 
         // Return data.
         $data = new stdClass();
-        if ($total) {
-            $data->id = $section->id;
-            $data->total = $total;
-            $data->complete = $complete;
-            $data->percentage = round(($complete / $total) * 100);
-            if ($data->percentage == 100) {
-                $data->done = true;
-            }
+        $data->id = $section->id;
+        $data->total = $total;
+        $data->complete = $complete;
+        $data->percentage = round(($complete / $total) * 100);
+        if ($data->percentage == 100) {
+            $data->done = true;
         }
         return $data;
     }

amd/src/local/toc.js
@@ -24,9 +24,9 @@
  */
 
 import {BaseComponent} from 'core/reactive';
-import CourseEvents from 'core_course/events';
 import {getCurrentCourseEditor} from 'core_courseformat/courseeditor';
 import {get_string as getString} from 'core/str';
+import Ajax from 'core/ajax';
 
 export default class Component extends BaseComponent {
 
@@ -64,59 +64,34 @@ export default class Component extends BaseComponent {
         sections.forEach((section) => {
             this.sections[section.dataset.id] = section;
         });
+    }
 
-        this._initSectionProgress();
+    getWatchers() {
+        return [
+            {watch: `section:deleted`, handler: this._deleteSection},
+            // Sections and cm sorting.
+            {watch: `course.sectionlist:updated`, handler: this._refreshCourseToc},
+            // Progress updates.
+            {watch: `cm.completionstate:updated`, handler: this._refreshCompletion},
+        ];
     }
 
     /**
-     * Section progress updates on load and when completion is toggled.
-     */
-    _initSectionProgress() {
-        const manualCompletionEvent = CourseEvents.manualCompletionEvent
-            || CourseEvents.manualCompletionToggled
-            || 'core_course:manualcompletiontoggled';
-
-        document.addEventListener(manualCompletionEvent, (event) => {
-            const detail = event.detail || {};
-            const cm = this.reactive.get('cm', detail.cmid);
-            if (!cm?.sectionid) {
-                return;
-            }
-
-            this._updateSectionProgress(cm.sectionid);
-        });
-
-        this.element.querySelectorAll('.progress-indicator[data-id]').forEach((indicator) => {
-            this._updateSectionProgress(indicator.dataset.id);
-        });
-    }
-
-    /**
-     * Count done items in one section, then pass the totals to the UI.
+     * Section progress updates when completion is toggled.
      *
-     * @param {string|number} sectionId the section id
+     * @param {Object} details the update details.
+     * @param {Object} details.element the element data.
      */
-    _updateSectionProgress(sectionId) {
-        const section = this.reactive.get('section', sectionId);
-        if (!section) {
-            return;
-        }
+    async _refreshCompletion({element}) {
+        const cm = this.reactive.get('cm', element.id);
+        const sectionId = cm.sectionid;
+        const progress = await Ajax.call([{
+            methodname: 'format_ucl_get_section_progress',
+            args: {sectionid: sectionId}
+        }])[0];
 
-        const cmlist = section.cmlist || [];
-        let total = 0;
-        let done = 0;
-
-        cmlist.forEach((cmId) => {
-            const cm = this.reactive.get('cm', cmId);
-            if (!cm || typeof cm.completionstate === 'undefined') {
-                return;
-            }
-
-            total++;
-            if (cm.isoverallcomplete === true || cm.completionstate === 1 || cm.completionstate === 2) {
-                done++;
-            }
-        });
+        const total = progress.total;
+        const done = progress.completed;
 
         this._renderProgress({sectionId, total, done});
     }
@@ -178,14 +153,6 @@ export default class Component extends BaseComponent {
                 clearInterval(interval);
             }
         }, 20);
-    }
-
-    getWatchers() {
-        return [
-            {watch: `section:deleted`, handler: this._deleteSection},
-            // Sections and cm sorting.
-            {watch: `course.sectionlist:updated`, handler: this._refreshCourseToc},
-        ];
     }
 
     /**

commit 2ea2300d716b8a44efbb6a1e108420cec278a56b

Ctp 4363 add section cancel (#57)
* CTP-6661 Fixed regression from merge conflict in PR #52

* CTP-4363 Added modal form for supplying section name and option to cancel

* CTP-4363 Added modal form for supplying section name and option to cancel

* CTP-4363 Added modal form for supplying section name and option to cancel

* CTP-4363 Added modal form for supplying section name and option to cancel

* CTP-4363 Added back hook to remove H1 on section page

* CTP-4363 Added back hook to remove H1 on section page

amd/src/addsection.js
@@ -1,0 +1,69 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Javascript module for adding a section with a name
+ *
+ * @module      format_ucl/addsection
+ * @copyright   2026 onwards University College London {@link https://www.ucl.ac.uk/}
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author      Stuart Lamour <s.lamour@ucl.ac.uk>
+ * @author      Amanda Doughty <m.doughty@ucl.ac.uk>
+ */
+
+import ModalForm from 'core_form/modalform';
+import Notification from 'core/notification';
+import {getString} from 'core/str';
+
+/**
+ * Initialize module
+ */
+export const init = () => {
+    const triggerElement = document.querySelector('.behat-add-section');
+
+    triggerElement.addEventListener('click', event => {
+        event.preventDefault();
+
+        const args = {
+            courseid: triggerElement.dataset.courseid,
+            sectionreturn: triggerElement.dataset.sectionreturn,
+        };
+
+        const modalForm = new ModalForm({
+            modalConfig: {
+                title: getString('addnewsection', 'format_ucl'),
+            },
+            formClass: 'format_ucl\\form\\addsection_form',
+            saveButtonText: getString('add', 'format_ucl'),
+            returnFocus: triggerElement,
+            args: args,
+        });
+
+        // Redirect to the new section when the form is submitted.
+        modalForm.addEventListener(modalForm.events.FORM_SUBMITTED, event => {
+            if (event.detail.result) {
+                window.location.assign(event.detail.url);
+            } else {
+                const warningMessages = event.detail.warnings.map(warning => warning.message);
+                Notification.addNotification({
+                    type: 'error',
+                    message: warningMessages.join('<br>')
+                });
+            }
+        });
+
+        modalForm.show();
+    });
+};

classes/courseformat/sectionactions.php
@@ -14,8 +14,13 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace format_ucl\courseformat;
+
+use core_courseformat\local\sectionactions as sectionactions_base;
+use stdClass;
+
 /**
- * Intermediate page to redirect to edit section page with new section ID
+ * Contains the core course state actions specific to ucl format.
  *
  * @package     format_ucl
  * @copyright   2026 onwards University College London {@link https://www.ucl.ac.uk/}
@@ -23,24 +28,17 @@
  * @author      Stuart Lamour <s.lamour@ucl.ac.uk>
  * @author      Amanda Doughty <m.doughty@ucl.ac.uk>
  */
-
-require_once("../../../config.php");
-
-global $PAGE, $DB;
-
-$courseid = required_param('course', PARAM_INT);
-$sectionnum = optional_param('section', null, PARAM_INT);
-
-$course = get_course($courseid);
-$params = ['course' => $course->id, 'section' => $sectionnum];
-$PAGE->set_url('/course/format/ucl/newsectionredirect.php', $params);
-
-if (!$section = $DB->get_record('course_sections', $params, '*')) {
-    redirect(course_get_url($course));
+class sectionactions extends sectionactions_base {
+    /**
+     * Creates a course section and adds it to the end
+     *
+     * This method returns a section record, not a section_info object. This prevents the regeneration
+     * of the modinfo object each time we create a section.
+     *
+     * @param stdClass|null $data
+     * @return stdClass created section object
+     */
+    public function create_from_form(?stdClass $data): stdClass {
+        return $this->create_from_object($data, true);
+    }
 }
-
-require_login($course);
-$context = context_course::instance($course->id);
-require_capability('moodle/course:update', $context);
-
-redirect(new moodle_url('/course/editsection.php', ['id' => $section->id]));

classes/form/addsection_form.php
@@ -1,0 +1,127 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace format_ucl\form;
+
+use context;
+use context_course;
+use core_courseformat\formatactions;
+use moodle_exception;
+use moodle_url;
+use core_form\dynamic_form;
+
+/**
+ * Add section modal form
+ *
+ * @package     format_ucl
+ * @copyright   2026 onwards University College London {@link https://www.ucl.ac.uk/}
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author      Stuart Lamour <s.lamour@ucl.ac.uk>
+ * @author      Amanda Doughty <m.doughty@ucl.ac.uk>
+ */
+class addsection_form extends dynamic_form {
+    /**
+     * Form definition
+     */
+    protected function definition() {
+        $mform  = $this->_form;
+
+        $defaultsectionname = get_string('sectiondefaultname', 'format_ucl');
+
+        $mform->addElement(
+            'text',
+            'name',
+            get_string('sectionname'),
+            [
+                'placeholder' => $defaultsectionname,
+                'size' => 30,
+                'maxlength' => 255,
+            ],
+        );
+        $mform->setType('name', PARAM_RAW);
+        $mform->addRule('name', get_string('maximumchars', '', 255), 'maxlength', 255, 'client');
+        $mform->addRule('name', null, 'required', null, 'client');
+
+        $mform->addElement('hidden', 'courseid', 0);
+        $mform->setType('courseid', PARAM_INT);
+    }
+
+    /**
+     * Return form context
+     *
+     * @return context
+     */
+    protected function get_context_for_dynamic_submission(): context {
+        $datasource = $this->_customdata ?? $this->_ajaxformdata;
+
+        return context_course::instance($datasource['courseid']);
+    }
+
+    /**
+     * Check if current user has access to this form, otherwise throw exception
+     *
+     * @throws moodle_exception
+     */
+    protected function check_access_for_dynamic_submission(): void {
+        $datasource = $this->_customdata ?? $this->_ajaxformdata;
+        $context = context_course::instance($datasource['courseid']);
+        require_capability('moodle/course:update', $context);
+    }
+
+    /**
+     * Process the form submission, used if form was submitted via AJAX
+     *
+     * @return array
+     */
+    public function process_dynamic_submission() {
+        global $CFG;
+        require_once($CFG->dirroot . '/course/lib.php');
+
+        $data = $this->get_data();
+        $section = formatactions::section($data->courseid)->create_from_form($data);
+        $returnurl = course_get_url($data->courseid, $section, ['sr' => $section->section]);
+        return [
+            'result' => true,
+            'url' => $returnurl->out(false),
+            'name' => get_section_name($data->courseid, $section),
+        ];
+    }
+
+    /**
+     * Load in existing data as form defaults (not applicable)
+     */
+    public function set_data_for_dynamic_submission(): void {
+        $datasource = $this->_customdata ?? $this->_ajaxformdata;
+        $data = (object)[
+            'courseid' => $datasource['courseid'],
+        ];
+        $this->set_data($data);
+    }
+
+    /**
+     * Returns url to set in $PAGE->set_url() when form is being rendered or submitted via AJAX
+     *
+     * @return moodle_url
+     */
+    protected function get_page_url_for_dynamic_submission(): moodle_url {
+        global $CFG;
+        require_once($CFG->dirroot . '/course/lib.php');
+
+        $datasource = $this->_customdata ?? $this->_ajaxformdata;
+
+        return course_get_url($datasource['courseid'], $this->optional_param('sectionreturn', 0, PARAM_INT));
+    }
+}

classes/local/hook_callbacks.php
@@ -50,17 +50,6 @@ class hook_callbacks {
             return;
         }
 
-        $newsectionredirect = new \moodle_url('/course/format/ucl/newsectionredirect.php');
-        if ($PAGE->has_set_url() && $PAGE->url->compare($newsectionredirect, URL_MATCH_BASE)) {
-            $sectionnum = $PAGE->url->get_param('section');
-            $courseid = $PAGE->url->get_param('courseid');
-            $modinfo = get_fast_modinfo($courseid);
-            if (!$sectioninfo = $modinfo->get_section_info($sectionnum)) {
-                redirect(new moodle_url('/course/view.php', ['id' => $courseid]));
-            }
-            redirect(new moodle_url('/course/editsection.php', ['id' => $sectioninfo->id]));
-        }
-
         $sectionurl = new \moodle_url('/course/section.php');
         if ($PAGE->has_set_url() && $PAGE->url->compare($sectionurl, URL_MATCH_BASE)) {
             $PAGE->set_heading('');

classes/output/widgets/toc.php
@@ -197,6 +197,9 @@ class toc implements renderable, templatable {
             'title' => "Add new section",
         ];
 
+        $data->courseid = $course->id;
+        $data->sectionreturn = $currentsectionnum;
+
         return $data;
     }
 

lang/en/format_ucl.php
@@ -26,6 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$string['add'] = 'Add';
 $string['addactivity'] = 'Add activity or resource';
 $string['addcustomcontact'] = 'Add custom contact';
 $string['adddescription'] = 'Add description';

lib.php
@@ -129,12 +129,7 @@ class format_ucl extends core_courseformat\base {
             return get_string('section0name', 'format_ucl');
         }
 
-        if (get_string_manager()->string_exists('sectiondefaultname', 'format_ucl')) {
-            return get_string('sectiondefaultname', 'format_ucl');
-        }
-
-        // Return an empty string if there's no available section name string for the given format.
-        return '';
+        return get_string('sectiondefaultname', 'format_ucl');
     }
 
     /**

templates/widgets/toc/toc.mustache
@@ -52,6 +52,8 @@
                 "progress": false
             }
         ],
+        "courseid": 1,
+        "sectionreturn": 0,
         "isediting": true,
         "addsections": {
             "url": "https://www.example.com/moodle/course/changenumsections.php?courseid=2&insertsection=0&sesskey=abc123"
@@ -71,7 +73,12 @@
       {{#isediting}}
         {{#addsections}}
         <div class="p-2 bg-primary-light text-center">
-          <a class="btn btn-add-content bg-white behat-add-section" href="{{{url}}}">
+          <a
+              class="btn btn-add-content bg-white behat-add-section"
+              href="{{{url}}}"
+              data-courseid="{{courseid}}"
+              data-sectionreturn="{{sectionreturn}}"
+          >
             <i class="fa-solid fa-circle-plus mr-2"></i>
             {{#str}} addnewsection, format_ucl {{/str}}
           </a>
@@ -85,5 +92,9 @@
 {{#js}}
   require(['format_ucl/local/toc'], function(component) {
         component.init('toc');
-    });
+  });
+
+  require(['format_ucl/addsection'], function(modal) {
+        modal.init();
+  });
 {{/js}}

tests/behat/add_and_edit_section.feature
@@ -1,8 +1,8 @@
-@format @format_ucl
-Feature: Adding a new section lands on the editing page
+@format @format_ucl @javascript
+Feature: Adding a new section opens a modal form requiring the user to submit a section name
   In order to add a new section
   As a user
-  I need to edit a new section when one is added
+  I need to include the section name first
 
   Background:
     Given the following "course" exists:
@@ -12,12 +12,35 @@ Feature: Adding a new section lands on the editing page
       | coursedisplay | 0        |
       | numsections   | 5        |
       | initsections  | 1        |
+    And the following "users" exist:
+      | username | firstname | lastname | email                | maildisplay |
+      | teacher1 | Teacher   | 1        | teacher1@example.com | 1           |
+    And the following "course enrolments" exist:
+      | user     | course | role           |
+      | teacher1 | C1     | editingteacher |
 
-  Scenario: Add and edit section
+  Scenario: User can cancel adding a section
     When I log in as "admin"
     And I am on "Course 1" course homepage with editing mode on
     And I click on ".behat-add-section" "css_element"
-    Then I should see "Edit section settings"
+    Then "Add a new section" "dialogue" should be visible
+    And I click on "Cancel" "button" in the "Add a new section" "dialogue"
+    And I should see "Introduction"
+
+  Scenario: User cannot add a new section without editing the name
+    When I log in as "admin"
+    And I am on "Course 1" course homepage with editing mode on
+    And I click on ".behat-add-section" "css_element"
+    Then "Add a new section" "dialogue" should be visible
+    And I click on "Add" "button" in the "Add a new section" "dialogue"
+    And "You must supply a value here" "text" should exist in the "Add a new section" "dialogue"
+
+  Scenario: User can add a new section after providing the section name
+    When I log in as "admin"
+    And I am on "Course 1" course homepage with editing mode on
+    And I click on ".behat-add-section" "css_element"
+    Then "Add a new section" "dialogue" should be visible
     And I set the field "Section name" to "Welcome to Stamptown"
-    And I press "Save changes"
-    And "Welcome to Stamptown" "link" should appear after "Section 5" "link"
+    And I click on "Add" "button" in the "Add a new section" "dialogue"
+    And I wait to be redirected
+    And "Welcome to Stamptown" "text" should appear after "Section 5" "link" in the "#toc" "css_element"

tests/behat/warnings.feature
@@ -112,8 +112,10 @@ Feature: Appropriate Tips are shown to user
     And ".behat-linktoguidance" "css_element" should not exist
 
     When I click on ".behat-add-section" "css_element"
-    And I press "Save changes"
-    Then ".behat-unnamedsections" "css_element" should exist
+    And I set the field "Section name" to "Welcome to Stamptown"
+    And I click on "Add" "button" in the "Add a new section" "dialogue"
+    And I am on "Course 1" course homepage
+    Then ".behat-unnamedsections" "css_element" should not exist
     And ".behat-toomanysections" "css_element" should exist
     And ".behat-toofewmods" "css_element" should exist
     And ".behat-linktoguidance" "css_element" should exist